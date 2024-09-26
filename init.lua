f7f7bf35d8cbca79e4110463b8b344ee42485dbd
Raymond Bian <raybbian@gmail.com>
2024-09-26 00:37:47 -0400

    prettierd

--- init.lua
+++ init.lua
@@ -560,7 +560,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -574,8 +574,9 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
-        typescript = { 'eslint', 'tsserver', stop_after_first = true },
-        typescriptreact = { 'eslint', 'tsserver', stop_after_first = true },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
       },
     },
   },
