e1f55a7ad03fccc638ae324c0cbf68ad325b54fa
Raymond Bian <raybbian@gmail.com>
2024-10-19 17:11:22 -0400

    tab stuff

--- init.lua
+++ init.lua
@@ -69,6 +69,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- tab width
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
